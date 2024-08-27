3106b4d649388dffa9b9efdb2dd048d63dafb03b
kdenny25 <kelsead25@gmail.com>
2024-08-27 08:04:31 -0400

    Added login modals and support for Google Identity.

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, request, render_template, redirect, session, jsonify
 from flask_login import LoginManager
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-config = load_dotenv('.env')
+config = dotenv_values('.env')
 
 # initialize Flask app
 app = Flask(__name__)
+
+app.secret_key = config['SECRET_KEY']
 
 # login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    user = None
+    if user is not None:
+        #if user.phone is Null:
+
+        return user
+    else:
+        return None
 
 @app.route('/')
 def hello_world():  # put application's code here
+    session['google_client'] = config['GOOGLE_LOGIN_CLIENTID']
     return render_template('layout.html')
 
 
